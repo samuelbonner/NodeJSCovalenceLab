9e15f53487b90e05f0dc42bb879f6072d18a92a1
Samuel Bonner <bonner.sam@gmail.com>
2021-07-12 10:47:53 -0500

    Part 2 actually finished. Throws an error in the popular-articles.json when ran. Need to investigate

--- reddit.js
+++ reddit.js
@@ -21,6 +21,6 @@ request("https://reddit.com/r/popular.json", (err, res, body) => {
   if (err) console.log(err);
 
   JSON.parse(body).data.children.forEach((item) => {
-    fs.appendFileSync(dataPath, item.data.title + "\n");
+    fs.appendFileSync(dataPath, [item.data.title, item.data.url, item.data.author] + "\n \n");
   });
 });
