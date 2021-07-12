e5a9c113f04e957597ceb960c8fd1887eb58d5a3
Samuel Bonner <bonner.sam@gmail.com>
2021-07-12 10:36:35 -0500

    Part 2 kinda finished. Throws an error in the popoular-articles.json when ran. Need to investigate

--- reddit.js
+++ reddit.js
@@ -1,17 +1,26 @@
-const rp = require('request-promise');
-const fs = require('fs');
+const rp = require("request-promise");
+const fs = require("fs");
+const request = require("request");
+const path = require("path");
+
+let dataPath = path.join(__dirname, "/popular-articles.json");
 
 var options = {
-    uri: 'https://reddit.com/r/popular.json',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
+  uri: "https://reddit.com/r/popular.json",
 };
 
-rp(options)
-    .then(function ($) {
-        // Process html like you would with jQuery...
-    })
-    .catch(function (err) {
-        // Crawling failed or Cheerio choked...
-    });
+// rp(options)
+//   .then(($) => {
+//     // Process html like you would with jQuery...
+//   })
+//   .catch((err) => {
+//     // Crawling failed...
+//   });
+
+request("https://reddit.com/r/popular.json", (err, res, body) => {
+  if (err) console.log(err);
+
+  JSON.parse(body).data.children.forEach((item) => {
+    fs.appendFileSync(dataPath, item.data.title + "\n");
+  });
+});
